5e683d7b21eca051f9e693003bf66c2572a0f295
Nitin Khanna <nitinthewiz@gmail.com>
2020-07-25 19:06:37 -0700

    let's just use double quotes

--- runner.sh
+++ runner.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "Screenshot John Hopkins Covid-19 ArcGIS page"
-node index.js -u 'https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6'
+node index.js -u "https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
 
 echo "Screenshot John Hopkins Covid-19 ArcGIS world map"
-node index.js -u 'https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6' -e 'div#esri.Map_2_root'
+node index.js -u "https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6" -e ".map"
 
 echo "Screenshot eleval observablehq div"
-node index.js -u 'https://observablehq.com/@elaval/coronavirus-worldwide-evolution' -e 'div#el2'
+node index.js -u "https://observablehq.com/@elaval/coronavirus-worldwide-evolution" -e "div#el2"
 
 echo "Screenshot eleval observablehq page"
-node index.js -u 'https://observablehq.com/@elaval/coronavirus-worldwide-evolution'
+node index.js -u "https://observablehq.com/@elaval/coronavirus-worldwide-evolution"
 
 echo "Screenshot worldspandemic World card"
-node index.js -u 'https://worldspandemic.com/'  -e '.cov19u-card'
+node index.js -u "https://worldspandemic.com/"  -e ".cov19u-card"
 
 echo "Screenshot worldspandemic Total World chart"
-node index.js -u 'https://worldspandemic.com/'  -e '.covid19-ultimate-evolution-chart'
+node index.js -u "https://worldspandemic.com/"  -e ".covid19-ultimate-evolution-chart"
 
 echo "Screenshot worldspandemic Daily New Cases World chart"
-node index.js -u 'https://worldspandemic.com/'  -e '.covid19-ultimate-daily-chart'
+node index.js -u "https://worldspandemic.com/"  -e ".covid19-ultimate-daily-chart"
 
 echo "zip the folder"
 zip -r screenshots.zip screenshots/
