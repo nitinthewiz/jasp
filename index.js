7393b37944bf009fd4b7666546179d6b345e131a
Nitin Khanna <nitinthewiz@gmail.com>
2020-07-29 13:32:55 -0700

    integrating inputs with Airtable
    Includes mobile view screenshots (but with an 88 px hack), added shelljs to run index.js, updated travis script to run node runner.js, better handling for div screenshots.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const ceil = require('ceil');
 const shortid = require('shortid');
 const Airtable = require('airtable');
 const puppeteer = require('puppeteer');
@@ -23,16 +24,29 @@ async function loadUrl(page, url) {
 	  .option('-u, --url <type>', 'the url to screenshot', 'https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6')
 	  .option('-t, --type <type>', 'the type of image. Options are jpeg and png. Default is png', 'png')
 	  .option('-e, --elem <type>', 'the dom element to focus on')
+	  .option('-v, --view <type>', 'the view type. Options are tv and mobile. Default is tv', 'tv')
+	  .option('-w, --width <type>', 'the width of the page')
+	  .option('-r, --row <type>', 'the airtable row ID.')
 	  .parse(process.argv);
 
 	// console.log(process.argv);
 	// console.log(`${program.type}`);
 
 
+	// set headless false for testing
+	// const browser = await puppeteer.launch({headless: false});
 	const browser = await puppeteer.launch();
+	
 
 	const page = await browser.newPage();
-	await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
+	VIEWPORT = { width: 1920, height: 1080, deviceScaleFactor: 2 };
+	if (program.width){
+		// the + 88 is an ugly hack. Make Nipun set the correct viewport width in airtable.
+		VIEWPORT = Object.assign({}, VIEWPORT, { width: ceil(Number(program.width) + 88) });
+		// VIEWPORT = { width: 1920, height: 1080, deviceScaleFactor: 2 };
+	}
+	// console.log(VIEWPORT);
+	await page.setViewport(VIEWPORT);
 
 	// await page.goto('https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6');
 
@@ -56,12 +70,48 @@ async function loadUrl(page, url) {
 
 		// await page.screenshot({path: 'screenshot.png'});
 		if (program.elem) {
+			// This code should ideally go outside this program.elem block, because, why
+			// not take a full screenshot of the entire page, right? But for some reason,
+			// it throws an error of 
+			// Protocol error (Page.captureScreenshot): Unable to capture screenshot
+			// for node index.js -u "https://observablehq.com/@elaval/coronavirus-worldwide-evolution"
+			// and so we'll have to figure that out at some later point.
+			// I can bet Nipun will ask for this at some point :D
+			// https://github.com/puppeteer/puppeteer/issues/2423#issuecomment-590738707
+			const fullPage = await page.$('body');
+			const fullPageSize = await fullPage.boundingBox();
+			// console.log(ceil(fullPageSize.height));
+		    await page.setViewport(
+		      Object.assign({}, VIEWPORT, { height: ceil(fullPageSize.height) })
+		    );
 			// const divElement = await page.$('div#ember61');
 			const domElement = await page.$(`${program.elem}`);
+			// this is redundant, but I'm keeping it here as an error
+			// check. If after the page content has loaded, this waitforselector
+			// doesn't return immediately, the script should fail.
+			await page.waitForSelector(`${program.elem}`);
+			// await page.evaluate((elem_to_move_to) => {
+			//    document.querySelector(elem_to_move_to).scrollIntoView();
+			// }, `${program.elem}`);
+
+			// waitForTimeout doesn't work in the current version of puppeteer.
+			// If we update the version, use this function instead of a normal timeout.
+			// await page.waitForTimeout(1000).then(() => console.log('Waited a second!'));
+			
 			if (`${program.type}` == 'jpeg'){
+				// await page.screenshot({
+		  //         clip: await domElement.boundingBox(),
+		  //         path: 'screenshots/screenshot_'+short_id+'.jpeg',
+		  //         quality: 100
+		  //       });
 				await domElement.screenshot({path: 'screenshots/screenshot_'+short_id+'.jpeg', quality: 100});
 			}
 			else{
+				// https://github.com/puppeteer/puppeteer/issues/2423#issuecomment-493188223
+				// await page.screenshot({
+		  //         clip: await domElement.boundingBox(),
+		  //         path: 'screenshots/screenshot_'+short_id+'.png'
+		  //       });
 				await domElement.screenshot({path: 'screenshots/screenshot_'+short_id+'.png'});
 			}
 		}
@@ -76,6 +126,7 @@ async function loadUrl(page, url) {
 			// await page.screenshot({path: 'screenshot.jpeg', quality: 100});	
 		}
 
+		// comment browser.close for testing
 		await browser.close();
 
 		if (program.elem) {
@@ -87,40 +138,64 @@ async function loadUrl(page, url) {
 
 		// console.log(filter_formula);
 
-		base('am_Embed').select({
-		    maxRecords: 1,
-		    filterByFormula: filter_formula
-		}).firstPage(function(err, records) {
-		    if (err) { console.error(err); return; }
-		    records.forEach(function(record) {
-		        // console.log('Retrieved', record.id);
-		        recordId = record.id;
+		var file_name = "";
+        if (`${program.type}` == 'jpeg'){
+			file_name = 'screenshot_'+short_id+'.jpeg';
+		}
+		else{
+			file_name = 'screenshot_'+short_id+'.png';
+		}
 
-		        var file_name = "";
-		        if (`${program.type}` == 'jpeg'){
-					file_name = 'screenshot_'+short_id+'.jpeg';
-				}
-				else{
-					file_name = 'screenshot_'+short_id+'.png';
-				}
-
-		        base('am_Embed').update([
-				  {
-				    "id": recordId,
-				    "fields": {
-				      "file_name": file_name
-				    }
-				  }
-				], function(err, records) {
-				  if (err) {
-				    console.error(err);
-				    return;
-				  }
-				});
-		    });
+		base('am_Embed').update([
+		  {
+		    "id": program.row,
+		    "fields": {
+		      "file_name": file_name
+		    }
+		  }
+		], function(err, records) {
+		  if (err) {
+		    console.error(err);
+		    return;
+		  }
 		});
+
+		// base('am_Embed').select({
+		//     maxRecords: 1,
+		//     filterByFormula: filter_formula
+		// }).firstPage(function(err, records) {
+		//     if (err) { console.error(err); return; }
+		//     records.forEach(function(record) {
+		//         // console.log('Retrieved', record.id);
+		//         recordId = record.id;
+
+		//         var file_name = "";
+		//         if (`${program.type}` == 'jpeg'){
+		// 			file_name = 'screenshot_'+short_id+'.jpeg';
+		// 		}
+		// 		else{
+		// 			file_name = 'screenshot_'+short_id+'.png';
+		// 		}
+
+		//         base('am_Embed').update([
+		// 		  {
+		// 		    "id": recordId,
+		// 		    "fields": {
+		// 		      "file_name": file_name
+		// 		    }
+		// 		  }
+		// 		], function(err, records) {
+		// 		  if (err) {
+		// 		    console.error(err);
+		// 		    return;
+		// 		  }
+		// 		});
+		//     });
+		// });
 	} catch (error) {
         console.log(error);
+        await browser.close();
+        // process.exit(1)
     }
 
 })();
---
+++ runner.js
@@ -0,0 +1,71 @@
+var shell = require('shelljs');
+const shortid = require('shortid');
+const Airtable = require('airtable');
+const puppeteer = require('puppeteer');
+
+var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.MEDIATER_BASE_ID);
+async function loadUrl(page, url) {
+    try {
+        await page.goto(url, {
+            timeout: 60000,
+            waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
+        });
+    } catch (error) {
+        throw new Error("url " + url + " url not loaded -> " + error);
+    }
+}
+
+async function launcher() {
+
+    filter_formula = 'AND({Animated} = "Static")';
+
+    await base('am_Embed').select({
+        // set maxRecords 1 for testing
+        // maxRecords: 1,
+        filterByFormula: filter_formula
+    }).firstPage(async function(err, records) {
+        if (err) { console.error(err); return; }
+        records.forEach(async function(record) {
+            // console.log(record);
+            // console.log(record.id);
+            // console.log(record.get("jasp_info_element"));
+            // console.log(record.get("jasp_info_url"));
+            recordId = record.id;
+
+            var run_cmd = ""
+
+            if (record.get("jasp_info_element")) {
+                run_cmd = "node index.js -u \"" + record.get("jasp_info_url") + "\" -e \"" + record.get("jasp_info_element") + "\"";
+            }
+            else {
+                run_cmd = "node index.js -u \"" + record.get("jasp_info_url") + "\"";
+            }
+
+            if (record.get("view_type") == "mobile") {
+                run_cmd = run_cmd + " -w \"" + record.get("dimension - width") + "\"";
+            }
+
+            run_cmd = run_cmd + " -r \"" + record.id + "\"";
+
+            // console.log(run_cmd);
+
+            // Run external tool synchronously
+            if (shell.exec(run_cmd).code !== 0) {
+              shell.echo('Error: node command failed');
+              shell.exit(1);
+            }
+
+        });
+        if (shell.exec("zip -r screenshots.zip screenshots/").code !== 0) {
+          shell.echo('Error: zip command failed');
+          shell.exit(1);
+        }
+        if (shell.exec("cp screenshots.zip /tmp/.").code !== 0) {
+          shell.echo('Error: zip copy command failed');
+          shell.exit(1);
+        }
+    });
+
+};
+
+launcher();
